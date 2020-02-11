1fefa2ce13cfbffa699fb0a2d3221b6196d16967
YosioSyoji <ningennototi95@gmail.com>
2020-02-11 19:01:17 +0900

    create

--- app/app.js
+++ app/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 const redis = require('redis');
-const client = redis.createClient();
+//const client = redis.createClient();
+const client = redis.createClient(6379,"redis");
 
 
 client.set("foo_rand000000000000000","OK");
