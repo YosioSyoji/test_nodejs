ad3908cd3b851f3fff162591ebc748c2ae38eb04
YosioSyoji <ningennototi95@gmail.com>
2020-02-11 21:19:20 +0900

    add ioredis

--- app/app.js
+++ app/app.js
@@ -1,19 +1,43 @@
 const express = require('express');
 const app = express();
 
-const redis = require('redis');
-//const client = redis.createClient();
-const client = redis.createClient(6379,"redis");
+const Redis = require('ioredis');
+const redis = new Redis(6379,"redis");
 
+var msg = "";
 
-client.set("foo_rand000000000000000","OK");
-client.get("foo_rand000000000000000",function(err,reply){
-	console.log(reply.toString());
+redis.set("foo","bar");
+redis.get("foo",function(err,result){
+	console.log(result);
 });
 
 
+// Or using a promise if the last argument isn't a function
+redis.get("foo")
+	.then(result => {
+	  console.log("res:"+result);
+	  msg = result;
+	})
+	.catch(err => {
+	  console.error("err:"+err);
+	});
+
+
+
+redis.del("foo");
+
+
+/*const redis = require('redis');
+const client = redis.createClient(6379,"redis");
+client.set("foo_rand000000000000000","OK");
+client.get("foo_rand000000000000000",function(err,reply){
+	console.log(reply.toString());
+});*/
+
+
 app.get('/',(req,res) => {
-	return res.send('test');
+	
+	return res.send(msg);
 });
 
 const PORT = 3000;
